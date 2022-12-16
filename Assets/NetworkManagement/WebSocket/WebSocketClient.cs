6d7c4c298b2bb29fd2e3081d0cb2d748c767b43e
ben051101@gmail.com <ben051101@gmail.com>
2022-12-16 11:05:27 +0100

    Change websocket

--- Assets/NetworkManagement/WebSocket/WebSocketClient.cs
+++ Assets/NetworkManagement/WebSocket/WebSocketClient.cs
@@ -4,6 +4,8 @@ using WebSocketSharp;
 public class WebSocketClient : MonoBehaviour
 {
     private WebSocket ws;
+    public string ip;
+    public string unrealIp;
 
     private void Start()
     {
@@ -23,7 +25,22 @@ public class WebSocketClient : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ws.Send("Hello");
+            SendMessage("Hello");
         }
+    }
+
+    public void TryReconnect()
+    {
+        ws = new WebSocket("ws://" + ip + ":8080");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+        };
+    }
+
+    private void SendMessage(string message)
+    {
+        ws.Send(message + "//" + unrealIp);
     }
 }
