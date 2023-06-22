348e5478a6941e961c98beff83f42de6ad884493
Benjamin FAVEROT <benjamin.faverot.auditeur@lecnam.net>
2023-06-22 16:20:36 +0200

    deux trois modifs

--- Assets/SocketIoClientTest.cs
+++ Assets/SocketIoClientTest.cs
@@ -2,49 +2,49 @@ using SocketIOClient;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 public class SocketIoClientTest : MonoBehaviour
 {
+    public static SocketIoClientTest Instance;
+    [SerializeField]
+    private string host;
+    [SerializeField]
+    private string RoomId;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance != null)
+            return;
+
+        Instance = this;
+    }
     void Start()
     {
         ConnectToServer();
     }
-
+    private void ParseCode(string code)
+    {
+        code = code.Remove(0, "{\"roomId\":\"".Count());
+        RoomId = code.Remove(7);
+    }
     private async void ConnectToServer()
     {
-        var client = new SocketIO("http://localhost:4000/");
+        var client = new SocketIO(host);
 
         client.On("message", response => {
-            Debug.Log(response);
+            if (response.ToString().Contains("roomId"))
+            {
+                ParseCode(response.GetValue<string>());
+            }
         });
-
-        client.On("hi", response =>
-        {
-            Debug.Log(response + " Oh yooo");
-
-            string text = response.GetValue<string>();
-
-        });
-
-        client.On("test", response =>
-        {
-            Debug.Log(response);
-
-            string text = response.GetValue<string>();
-            var dto = response.GetValue<TestDTO>(1);
-
-        });
-
-        client.OnConnected += async (sender, e) =>
-        {
-            await client.EmitAsync("message", "socket.io");
-
-            var dto = new TestDTO { Id = 123, Name = "bob" };
-            await client.EmitAsync("register", "source", dto);
-        };
         await client.ConnectAsync();
+
+        
     }
+
 }
 
 internal class TestDTO
